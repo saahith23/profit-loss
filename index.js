020364aa9800cbd5b6c93f8aa66b7cc9be65b01c
saahith23 <saahithvaspari23@gmail.com>
2021-09-24 21:16:59 +0530

    .

---
+++ index.js
@@ -0,0 +1,36 @@
+var initialPrice = document.querySelector("#initial-price");
+var stocksQuantity = document.querySelector("#quantity");
+var currentPrice = document.querySelector("#current-price");
+var submitBtn = document.querySelector("#button");
+var output = document.querySelector("#output");
+
+submitBtn.addEventListener('click', submitHandler);
+
+function submitHandler(){
+    var ip= Number(initialPrice.value);
+    var qty=Number( stocksQuantity.value);
+    var cp= Number(currentPrice.value);
+
+    calculateProfitAndLoss(ip, qty,cp);
+}
+
+function calculateProfitAndLoss(initial,quantity, current)
+{
+    if(initial > current){
+var loss =(initial - current)*quantity;
+var lossPercentage = (loss/initial)*100;
+
+showOutput('h}${}')
+    } 
+    else if(current > initial){
+var profit = (current - initial)*quantity;
+var profitPercentage= (profit/initial)*100;
+showOutput('h}${}jhgibk')
+    }
+    else{
+showOutput('..')
+    }
+}
+function showOutput(message){
+output.innerHTML = message;
+}
